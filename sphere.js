41e10e9a8b9a869c3b5716b946581e56c710c6a4
ilias ioannou <ioannou@planetek.it>
2025-02-17 13:18:27 +0200

    Add WEBGL spheres

---
+++ sphere.js
@@ -0,0 +1,26 @@
+import Spheres1Background from 'https://cdn.jsdelivr.net/npm/threejs-components@0.0.17/build/backgrounds/spheres1.cdn.min.js'
+// import Spheres1Background from './js/sphere.min.js'
+
+
+const bg = Spheres1Background(document.getElementById('webgl-canvas'), {
+  count: 300,
+  minSize: 0.3,
+  maxSize: 1,
+  gravity: 0.5
+})
+
+// document.body.addEventListener('click', () => {
+//   bg.spheres.setColors([0xffffff * Math.random(), 0xffffff * Math.random(), 0xffffff * Math.random()])
+// })
+
+//document.body.addEventListener('keydown', (ev) => {
+//  bg.spheres.config.gravity = bg.spheres.config.gravity === 0 ? 1 : 0
+//})
+
+document.getElementById('gravity-btn').addEventListener('click', () => {
+  bg.spheres.config.gravity = bg.spheres.config.gravity === 0 ? 1 : 0
+})
+
+document.getElementById('colors-btn').addEventListener('click', () => {
+  bg.spheres.setColors([0xffffff * Math.random(), 0xffffff * Math.random(), 0xffffff * Math.random()])
+})
